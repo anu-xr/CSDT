e9bf2a7a3f4f84f337c533a3f75a37cb25c75a61
ryota-mitarai <ryotamitarai0@gmail.com>
2021-06-11 20:50:04 -0400

    chore: switch to parcel

--- src/base.js
+++ src/base.js
@@ -1,13 +1,12 @@
 import * as Y from 'yjs';
 
 //base CSDT object
-//both the parent and child implementation extend from this
 export default class Base {
   constructor() {
     this.version = '0.1.0';
     this.ydoc = new Y.Doc();
 
-    //ydoc recieve updates
+    //receive ydoc updates
     document.addEventListener('CSDT-y-update', (e) => {
       const update = e.detail;
       Y.applyUpdate(this.ydoc, update);
